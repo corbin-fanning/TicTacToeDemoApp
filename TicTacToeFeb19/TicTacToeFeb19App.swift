36a67d279e0ebd86aa6d8fcc1e8a23e61c92a134
Corbin Fanning <corbin.hfanning@tamu.edu>
2024-02-19 14:37:42 -0600

    Initial Commit

---
+++ TicTacToeFeb19/TicTacToeFeb19App.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeFeb19App.swift
+//  TicTacToeFeb19
+//
+//  Created by Fanning, Corbin on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeFeb19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
